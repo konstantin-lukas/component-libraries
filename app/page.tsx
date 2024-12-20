2aa2ab49211a61617d06139f9bb829d09830bb12
konstantin <konstantin_lukas@web.de>
2024-12-20 14:39:41 +0100

    update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,6 @@ export default function Home() {
         <>
             <h1 className="font-bold text-2xl uppercase">React Component Libraries</h1>
             <p>
-                This site implements a regular form using different component libraries in a project using tailwindCSS.
-                It includes the following features:
-            </p>
-            <ul>
-                <li>
-                    Two buttons with different styles for submitting and populating the input fields
-                </li>
-            </ul>
-            <br/>
-            <p>
                 Navigate to any of the pages below to see the implemented form for each library. Each page is using the
                 same types, reducers and event handlers.
             </p>
